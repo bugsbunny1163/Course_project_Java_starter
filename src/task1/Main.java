22ad975cfef7311584048a5b9da39f89eed2692a
Volodymyr <bugsbunny1163@gmail.com>
2022-05-01 13:48:17 +0300

    task 1 done

---
+++ src/task1/Main.java
@@ -0,0 +1,32 @@
+package task1;
+
+public class Main {
+    public static void main(String[] args) {
+        int var = -864542;
+        System.out.println(digitsSum(var));
+
+    }
+
+    static int digitsSum(int var) {
+        int tempVar = Math.abs(var);
+        int length = 0;
+        long temp = 1;
+        while (temp <= tempVar) {
+            length++;
+            temp *= 10;
+        }
+        // закидуємо кожну цифру окремо у масив і сумуємо їх
+        int[] array = new int[length];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = tempVar % 10;
+            tempVar = tempVar / 10;
+        }
+        int sum = 0;
+        for (int j : array) {
+            sum += j;
+        }
+        return sum;
+    }
+
+
+}

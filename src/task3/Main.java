b2a58c82ebefbeb8aeaa004a063e62fee4e6e83e
Volodymyr <bugsbunny1163@gmail.com>
2022-06-24 13:32:15 +0300

    task 3 done

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -3,5 +3,18 @@ package task3;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(numberOfName("Roman"));
+
+    }
+
+    private static int numberOfName(String name) {
+        String al = "abcdefghijklmnopqrstuvwxyz";
+        String nameLowerCase = name.toLowerCase();
+        int sum = 0;
+        for (int i = 0; i < nameLowerCase.length(); i++) {
+            int currentNumberOfChar = al.indexOf(nameLowerCase.charAt(i) + 1);
+            sum += currentNumberOfChar;
+        }
+        return sum;
     }
 }
